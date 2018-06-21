0da159b125231155b56cd65d67011f12e9de7488
Judy McGoldrick <jjeanius@verizon.net>
2018-06-21 14:43:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/" do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    "200 status code"
+
+    erb :piglatinize
+  end
+
+  post '/piglatinize' do
+    @@text = PigLatinizer.new(params[:user_phrase])
+
+    erb :piglatinize
+  end
+
 end
