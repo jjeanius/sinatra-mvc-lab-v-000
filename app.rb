8ef9de2ffb6a16ed1f2988752168917b096e24b3
Judy McGoldrick <jjeanius@verizon.net>
2018-06-21 14:52:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,14 +3,20 @@ require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
 
-  get '/' do
+  get "/" do
     erb :user_input
   end
 
   post '/piglatinize' do
-    @piglatin_text = PigLatinizer.new.piglatinize(params[:user_phrase])
+    "200 status code"
 
-    erb :results
+    erb :piglatinize
+  end
+
+  post '/piglatinize' do
+    @@text = PigLatinizer.new(params[:user_phrase])
+
+    erb :piglatinize
   end
 
 end
